f5cbbe0b3a7d9f12735085afd8a3b0e8ac6fa489
Aaron Christopher Ramirez <aramirez.4@hotmail.com>
2024-09-22 20:07:47 -0700

    CREATE, UPDATE, DELETE Operations
    API contains the code and methods for CREATE, UPDATE, and DELETE requests for the Product Class.

--- app/src/main/java/com/ims/app/controller/ProductController.java
+++ app/src/main/java/com/ims/app/controller/ProductController.java
@@ -4,8 +4,7 @@ import com.ims.app.model.Product;
 
 import com.ims.app.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,8 +15,27 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    // Get All Products
     @RequestMapping("/product")
     public List<Product> GetAllProducts(){
         return productService.getAllProducts();
+    }
+
+    // Create POST request
+    @RequestMapping(method = RequestMethod.POST, value = "/product")
+    public void addProduct(@RequestBody Product product){
+        productService.addProduct(product);
+    }
+
+    // Create UPDATE request
+    @RequestMapping(method = RequestMethod.PUT, value = "/product/{id}")
+    public void updateProduct(@RequestBody Product product, @PathVariable String id){
+        productService.updateProduct(id, product);
+    }
+
+    // create DELETE request
+    @RequestMapping(method = RequestMethod.DELETE, value = "/product/{id}")
+    public void deleteProduct(@PathVariable String id){
+        productService.deleteProduct(id);
     }
 }
--- app/src/main/java/com/ims/app/service/ProductService.java
+++ app/src/main/java/com/ims/app/service/ProductService.java
@@ -3,19 +3,42 @@ package com.ims.app.service;
 import com.ims.app.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    private List<Product> products = Arrays.asList(
+    private List<Product> products = new ArrayList<>(Arrays.asList(
             new Product("1", "Xbox", "Microsoft", "Series-X", 500.00, 1000),
                 new Product("2", "Playstation 5", "Sony", "Pro", 800.00, 1200),
                 new Product("3", "Nintendo Switch", "Nintendo", "Switch", 299.99, 800)
-        );
+        ));
 
+    // Get All products
     public List<Product> getAllProducts(){
         return products;
+    }
+
+    // POST method
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    // UPDATE method
+    public void updateProduct(String id, Product product) {
+        for(int i = 0; i < products.size(); i++){
+            Product p = products.get(i);
+            if (p.getID().equals(id)){
+                products.set(i, product);
+                return;
+            }
+        }
+    }
+
+    // DELETE method
+    public void deleteProduct(String id) {
+        products.removeIf(p -> p.getID().equals(id));
     }
 }
